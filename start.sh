25ee9a8ac4a3d1f4fb80c557e51ea587898ad183
pwcong <pwcong@foxmail.com>
2021-01-27 11:18:40 +0800

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-echo "Adding $TARGET to the NGINX config"
+echo "Adding prefix '$PREFIX' and target '$TARGET' to the NGINX config"
+sed -i -e "s,__PREFIX__,$PREFIX,g" /etc/nginx/conf.d/default.conf
 sed -i -e "s,__TARGET__,$TARGET,g" /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
